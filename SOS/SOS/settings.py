385e88a5ff56793f58504a1ccdd7d8ed4ef30131
VIshal6620 <vishalvish7099@gmail.com>
2025-12-26 14:20:14 +0530

    26-12-2025

--- SOS/SOS/settings.py
+++ SOS/SOS/settings.py
@@ -77,15 +77,14 @@ WSGI_APPLICATION = 'SOS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-
 DATABASES = {
-    'default': {
+     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('DB_NAME', 'project-20'),
-        'USER': os.getenv('DB_USER', 'root'),
-        'PASSWORD': os.getenv('DB_PASSWORD', 'root'),
-        'HOST': os.getenv('DB_HOST', 'db'),
-        'PORT': os.getenv('DB_PORT', '3306'),
+        'NAME': 'project-20',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
@@ -136,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vishalvish@gmail.com'
+EMAIL_HOST_USER = 'vishalvish7099@gmail.com'
 EMAIL_HOST_PASSWORD = 'spfo skqf fnjo gfyd'
